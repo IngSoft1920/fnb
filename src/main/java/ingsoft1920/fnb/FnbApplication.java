6496725600fe8efa85726a568b85b4d3aafb79db
Jorge Vazquez <jorge.vazquez.acevedo@alumnos.upm.es>
2020-01-31 15:42:46 +0100

    Incluir Logger + Test ejemplo

--- src/main/java/ingsoft1920/fnb/FnbApplication.java
+++ src/main/java/ingsoft1920/fnb/FnbApplication.java
@@ -1,12 +1,16 @@
 package ingsoft1920.fnb;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FnbApplication {
+	final static Logger logger = LogManager.getLogger(FnbApplication.class.getName());
 
 	public static void main(String[] args) {
+		logger.warn("Aplicacion iniciada");
 		SpringApplication.run(FnbApplication.class, args);
 	}
 
--- src/test/java/ingsoft1920/fnb/FnbApplicationTests.java
+++ src/test/java/ingsoft1920/fnb/FnbApplicationTests.java
@@ -1,5 +1,7 @@
 package ingsoft1920.fnb;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,16 @@ class FnbApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void testEjemploCorrecto() {
+		assertEquals(1, 1);
+	}
+	
+	@Test
+	void testEjemploIncorrecto() {
+		assertEquals(0, 1);
 	}
 
 }
