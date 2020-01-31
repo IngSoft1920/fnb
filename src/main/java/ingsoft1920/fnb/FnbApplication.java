f0627c15504eef6de4c5981aa107074987e70321
Jorge Vazquez <jorge.vazquez.acevedo@alumnos.upm.es>
2020-01-31 09:35:45 +0100

    Setup Spring

---
+++ src/main/java/ingsoft1920/fnb/FnbApplication.java
@@ -0,0 +1,13 @@
+package ingsoft1920.fnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FnbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ingsoft1920/fnb/FnbApplicationTests.java
@@ -0,0 +1,13 @@
+package ingsoft1920.fnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
