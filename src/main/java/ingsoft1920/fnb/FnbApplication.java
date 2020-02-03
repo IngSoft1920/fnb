04b2002ccfb3df6cb8d5e4dcba39cfc3e9f9b51d
Jorge Vazquez <jorge.vazquez.acevedo@alumnos.upm.es>
2020-02-03 13:21:41 +0100

    Fix test, controller y logger

--- src/main/java/ingsoft1920/fnb/FnbApplication.java
+++ src/main/java/ingsoft1920/fnb/FnbApplication.java
@@ -4,9 +4,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class FnbApplication {
 	final static Logger logger = LogManager.getLogger(FnbApplication.class.getName());
--- src/test/java/ingsoft1920/fnb/FnbApplicationTests.java
+++ src/test/java/ingsoft1920/fnb/FnbApplicationTests.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class FnbApplicationTests {
 
 	@Test
