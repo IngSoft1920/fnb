77477f56be685c124c95920b5653cf39b91ec6ee
Alejandro Garcia <a.gcastellanos@alumnos.upm.es>
2020-02-18 15:31:45 +0100

    test push develop

--- src/main/java/ingsoft1920/fnb/FnbApplication.java
+++ src/main/java/ingsoft1920/fnb/FnbApplication.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+//test
+
+
 @Controller
 @SpringBootApplication
 public class FnbApplication {
